d893bba11378ef86f3ab19173459fe7ff0450ff8
Jens Saade <jens@verticle.io>
2024-11-27 13:48:40 +0100

    fix import

--- src/main/java/io/platformspec/crd/provider/Spec.java
+++ src/main/java/io/platformspec/crd/provider/Spec.java
@@ -1,9 +1,9 @@
 package io.platformspec.crd.provider;
 
 import io.fabric8.crd.generator.annotation.PreserveUnknownFields;
-import io.fabric8.kubernetes.api.model.Config;
 import io.fabric8.kubernetes.api.model.ObjectReference;
 import io.platformspec.crd.provider.spec.Categories;
+import io.platformspec.crd.provider.spec.Config;
 
 public class Spec {
 
