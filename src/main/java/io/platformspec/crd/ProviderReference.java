f62e89f3da2deb9249fa7ec298e5b45f85a2f1a3
Jens Saade <jens@verticle.io>
2025-02-03 22:40:18 +0100

    reference annotations

---
+++ src/main/java/io/platformspec/crd/ProviderReference.java
@@ -0,0 +1,8 @@
+package io.platformspec.crd;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProviderReference {
+}
