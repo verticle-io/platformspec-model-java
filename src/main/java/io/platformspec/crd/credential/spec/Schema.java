227a7cedb10ca7e1099a95d701844861f1a49568
Jens Saade <jens@verticle.io>
2025-03-27 21:57:35 +0100

    + AZURE

--- src/main/java/io/platformspec/crd/credential/spec/Schema.java
+++ src/main/java/io/platformspec/crd/credential/spec/Schema.java
@@ -3,5 +3,6 @@ package io.platformspec.crd.credential.spec;
 public enum Schema {
 
     AWS,        //	AWS API access credentials
-    GCP         //	GCP API access credentials
+    GCP,         //	GCP API access credentials
+    AZURE
 }
